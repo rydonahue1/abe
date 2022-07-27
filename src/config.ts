e9052f882c1b0731856ee6a4644ad681ce57ff42
Ryan Donahue <ryan.donahue@rhsps.com>
2022-07-27 18:19:57 -0400

    added youtube key

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,4 @@ import config from "../config.json"
 export const token = config.token;
 export const firebaseAdmin = config.firebase_admin;
 export const firebaseBucket = config.firebase_bucket;
+export const youtubeKey = config.youtubeKey
